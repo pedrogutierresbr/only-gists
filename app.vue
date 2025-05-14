8578f54053c578cc7265ea85e24864ce0be94107
pedrogutierresbr <pedro-gabriel1@hotmail.com>
2025-05-14 01:41:20 -0300

    bootstrap

---
+++ app.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1 class="font-inter-500 text-red-300">Only Gists</h1>
+    <Button label="Submit" />
+  </div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,25 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  modules: ['@primevue/nuxt-module', '@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
+  css: ['primeicons/primeicons.css', '~/assets/css/tailwind.css'],
+  googleFonts: {
+    base64: true,
+    fontsDir: 'assets/fonts',
+    overwriting: true,
+    families: {
+      Inter: [300, 500, 800],
+    },
+  },
+  primevue: {
+    options: { unstyled: true },
+    importPT: {
+      as: 'lara',
+      from: '@/assets/presets/lara/',
+    },
+    components: {
+      exclude: ['Form', 'FormField'],
+    },
+  },
+})
