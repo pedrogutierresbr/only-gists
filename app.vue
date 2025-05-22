ce794a7ccb940df34a228bbd97205fae8ba02f61
pedrogutierresbr <pedro-gabriel1@hotmail.com>
2025-05-22 20:39:39 -0300

    criado primeiro modulo e inicializado layout

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <h1 class="font-inter-500 text-red-300">Only Gists</h1>
-    <Button label="Submit" />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
---
+++ modules/landing-page/screens/Home/Home.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Teste</h1>
+</template>
