a7c0ce04a71c46ad81e086b5078227f4c1f8c69e
pedrogutierresbr <pedro-gabriel1@hotmail.com>
2025-05-24 20:11:16 -0300

    criado logo e HOC mainContent

--- modules/landing-page/screens/Home/Home.vue
+++ modules/landing-page/screens/Home/Home.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-import Header from '@/modules/landing-page/components/Header/Header.vue';
-import Hero from '@/modules/landing-page/components/Hero/Hero.vue';
+import Header from '@/modules/landing-page/components/Header/Header.vue'
+import Hero from '@/modules/landing-page/components/Hero/Hero.vue'
 </script>
 
 <template>
-  <Header />
-  <Hero />
+  <MainContent>
+    <template #header>
+      <Header />
+    </template>
+
+    <template #content>
+      <Hero />
+    </template>
+  </MainContent>
 </template>
