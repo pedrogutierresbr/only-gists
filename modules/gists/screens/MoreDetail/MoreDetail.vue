15541f2dd12d09974182a27a3d459853566cfdc0
pedrogutierresbr <pedro-gabriel1@hotmail.com>
2025-05-29 22:52:19 -0300

    fianalizado DialogPaymentError e inserido Lazy para import dinamico nos modais

--- modules/gists/screens/MoreDetail/MoreDetail.vue
+++ modules/gists/screens/MoreDetail/MoreDetail.vue
@@ -1,16 +1,22 @@
 <script setup lang="ts">
 import PublicHeadline from '@/modules/gists/components/PublicHeadline/PublicHeadline.vue'
 import GistCodeSnippet from '@/modules/gists/components/CodeSnippet/CodeSnippet.vue'
-import DialogPaymentSuccess from '@/modules/payments/components/DialogPaymentSuccess/DialogPaymentSuccess.vue'
+import LazyDialogPaymentSuccess from '@/modules/payments/components/DialogPaymentSuccess/DialogPaymentSuccess.vue'
+import LazyDialogPaymentError from '@/modules/payments/components/DialogPaymentError/DialogPaymentError.vue'
 
 const route = useRoute()
 const isPaymentSuccessfully = ref<boolean>(false)
+const isPaymentFail = ref<boolean>(false)
 
 onMounted(() => {
-  const { success_payment } = route.query
+  const { success_payment, fail_payment } = route.query
 
   if (success_payment) {
     isPaymentSuccessfully.value = true
+  }
+
+  if (fail_payment) {
+    isPaymentFail.value = true
   }
 })
 </script>
@@ -19,5 +25,6 @@ onMounted(() => {
   <PublicHeadline />
 
   <GistCodeSnippet />
-  <DialogPaymentSuccess v-model:visible="isPaymentSuccessfully" />
+  <LazyDialogPaymentSuccess v-model:visible="isPaymentSuccessfully" />
+  <LazyDialogPaymentError v-model:visible="isPaymentFail" />
 </template>
