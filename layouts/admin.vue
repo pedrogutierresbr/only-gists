238f5830fd1d791a117078ae2c1965d8a153d290
pedrogutierresbr <pedro-gabriel1@hotmail.com>
2025-06-17 21:43:06 -0300

    criado estrutura de layout admin e utilizado middleware auth

---
+++ layouts/admin.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-full h-full flex flex-col items-center">
+    <MainContent>
+      <template #header>Header</template>
+      <template #content>
+        <slot />
+      </template>
+    </MainContent>
+  </div>
+</template>
---
+++ pages/app/panel.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Panel from '@/modules/reports/screens/Panel/Panel.vue'
+
+definePageMeta({
+  layout: 'admin',
+  middleware: ['auth']
+})
+</script>
+
+<template>
+  <Panel />
+</template>
