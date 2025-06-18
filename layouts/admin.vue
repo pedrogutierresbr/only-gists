dc76239a0471f84f84a1f17ada2de8750478b1c0
pedrogutierresbr <pedro-gabriel1@hotmail.com>
2025-06-17 22:27:18 -0300

    criado header do mod auth com navegacao user logado

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -1,7 +1,33 @@
+<script setup lang="ts">
+import Header from '@/modules/auth/components/Header/Header.vue'
+import { useSession } from '@/modules/auth/composables/useSession/useSession'
+
+const router = useRouter()
+const session = useSession()
+
+const handleLogout = async () => {
+  const { error } = await session.logout()
+
+  if (!error) {
+    router.push('/')
+  }
+}
+</script>
+
 <template>
   <div class="w-full h-full flex flex-col items-center">
     <MainContent>
-      <template #header>Header</template>
+      <template #header>
+        <Header
+          profile-pic="teste"
+          nickname="nickname"
+          @navigate-to-new-gist="() => router.push('/app/gist/create')"
+          @navigate-to-profile-edit="() => router.push('/app/profile/edit')"
+          @navigate-to-sales="() => router.push('/app/sales/all')"
+          @navigate-to-reports="() => router.push('/app/panel')"
+          @logout="handleLogout()"
+        />
+      </template>
       <template #content>
         <slot />
       </template>
---
+++ modules/auth/components/Header/Header.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import { separator } from '@primeuix/themes/aura/breadcrumb'
+import { label } from '@primeuix/themes/aura/metergroup'
+
+const emit = defineEmits<{
+  (e: 'navigate-to-new-gist'): void
+  (e: 'navigate-to-reports'): void
+  (e: 'navigate-to-profile-edit'): void
+  (e: 'navigate-to-sales'): void
+  (e: 'logout'): void
+}>()
+
+const props = defineProps<{
+  profilePic?: string
+  nickname: string
+}>()
+
+const menu = ref()
+
+const items = [
+  {
+    label: 'Painel',
+    icon: 'pi pi-chart-line',
+    command: () => emit('navigate-to-reports'),
+  },
+  {
+    label: 'Novo gist',
+    icon: 'pi pi-plus',
+    command: () => emit('navigate-to-new-gist'),
+  },
+  {
+    label: 'Editar perfil',
+    icon: 'pi pi-user',
+    command: () => emit('navigate-to-profile-edit'),
+  },
+  {
+    label: 'Vendas',
+    icon: 'pi pi-money-bill',
+    command: () => emit('navigate-to-sales'),
+  },
+  {
+    separator: true,
+  },
+  {
+    label: 'Sair',
+    icon: 'pi pi-sign-out',
+    command: () => emit('logout'),
+  },
+]
+
+const toggle = (event: Event) => {
+  menu.value?.toggle(event)
+}
+</script>
+
+<template>
+  <header class="w-full shadow">
+    <nav class="bg-white px-4 lg:px-6 py-2.5">
+      <div class="flex justify-between items-center mx-auto max-w-screen-xl">
+        <div class="flex items-center">
+          <nuxt-link to="/app/panel">
+            <Logo />
+          </nuxt-link>
+        </div>
+
+        <div class="flex items-center">
+          <button class="flex gap-2 items-center" aria-haspopup="true" aria-controls="header-auth-menu" @click="toggle">
+            <span class="font-[Inter] font-regular text-lg text-gray-700">Olá, {{ props.nickname }}!</span>
+            <div class="w-9 h-9 rounded-full overflow-hidden" v-if="props.profilePic">
+              <img :src="props.profilePic" alt="foto de perfil do usuário" />
+            </div>
+          </button>
+        </div>
+
+        <Menu ref="menu" id="header-auth-menu" :model="items" :popup="true"></Menu>
+      </div>
+    </nav>
+  </header>
+</template>
