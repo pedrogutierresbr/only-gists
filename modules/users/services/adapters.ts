fc44dffc4cde3601fbdb14a2a4b8d55e48f98bed
pedrogutierresbr <pedro-gabriel1@hotmail.com>
2025-07-10 01:31:29 -0300

    Serviço de busca por cep integrado com ViaCep

--- modules/users/services/adapters.ts
+++ modules/users/services/adapters.ts
@@ -1,9 +1,22 @@
 import type { User } from '@/modules/users/entities/User/User'
 import type { Database } from '@/libs/supabase/schema'
 import type { Address } from '@/modules/users/entities/Address/Address'
+import type { SearchAddressResponse } from './types'
 
 type ProfileTable = Database['public']['Tables']['profiles']
 type Row = ProfileTable['Row']
+
+export function searchAddressByZipCodeAdapter(data: SearchAddressResponse): Address {
+  return {
+    zipCode: data.cep,
+    state: data.uf,
+    city: data.localidade,
+    street: data.logradouro,
+    complement: data.complemento,
+    neighborhood: data.bairro,
+    number: ''
+  }
+}
 
 export function getMyselfAdapter(data: Row | null): User | null {
   if(!data) {
--- modules/users/services/services.ts
+++ modules/users/services/services.ts
@@ -1,8 +1,16 @@
 import type { SupabaseClient } from '@supabase/supabase-js'
 import type { Database } from '@/libs/supabase/schema'
-import { getMyselfAdapter } from './adapters'
+import { getMyselfAdapter, searchAddressByZipCodeAdapter } from './adapters'
+import type { AxiosInstance } from 'axios'
+import type { SearchAddressResponse } from './types'
 
-export default (client: SupabaseClient<Database>) => ({
+export default (client: SupabaseClient<Database>, httpClient: AxiosInstance) => ({
+  async searchAddressByZipCode(zipCode: string) {
+    const url = `https://viacep.com.br/ws/${zipCode}/json/`
+    const response = await httpClient.get<SearchAddressResponse>(url)
+    const address = searchAddressByZipCodeAdapter(response.data)
+  },
+
   async getMyself(id: string) {
     const response = await client.from('profiles').select('*').eq('id', id).single()
     const user = getMyselfAdapter(response.data)
