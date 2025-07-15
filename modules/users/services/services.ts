4dfc4efb3f700155be7fbb18907e5e66991c5c34
pedrogutierresbr <pedro-gabriel1@hotmail.com>
2025-07-15 20:25:14 -0300

    Criando serviço de update de user

--- modules/users/services/services.ts
+++ modules/users/services/services.ts
@@ -1,5 +1,6 @@
 import type { SupabaseClient } from '@supabase/supabase-js'
 import type { Database } from '@/libs/supabase/schema'
+import type { User } from '@/modules/users/entities/User/User'
 import { getMyselfAdapter, searchAddressByZipCodeAdapter } from './adapters'
 import type { AxiosInstance } from 'axios'
 import type { SearchAddressResponse } from './types'
@@ -10,7 +11,7 @@ export default (client: SupabaseClient<Database>, httpClient: AxiosInstance) => 
     const response = await httpClient.get<SearchAddressResponse>(url)
     const address = searchAddressByZipCodeAdapter(response.data)
     return {
-      data: address
+      data: address,
     }
   },
 
@@ -18,5 +19,28 @@ export default (client: SupabaseClient<Database>, httpClient: AxiosInstance) => 
     const response = await client.from('profiles').select('*').eq('id', id).single()
     const user = getMyselfAdapter(response.data)
     return user
+  },
+
+  async update(id: string, { name, site, bio, phone, address }: User) {
+    await client
+      .from('profiles')
+      .update({
+        name,
+        site,
+        bio,
+        phone,
+        addres: {
+          zipCode: address?.zipCode,
+          number: address?.number,
+          street: address?.street,
+          city: address?.city,
+          state: address?.state,
+          neightborhood: address?.neighborhood,
+          complement: address?.complement,
+        },
+      })
+      .eq('id', id)
+
+    return { id }
   },
 })
