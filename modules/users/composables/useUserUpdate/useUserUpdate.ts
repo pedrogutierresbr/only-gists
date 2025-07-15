623e05d1a29c88a38c5d63e0c75b95936e3377c7
pedrogutierresbr <pedro-gabriel1@hotmail.com>
2025-07-15 20:50:13 -0300

    criado hook useUserUpdate() com validação via Zod

---
+++ modules/users/composables/useUserUpdate/useUserUpdate.ts
@@ -0,0 +1,70 @@
+import { z } from 'zod'
+import type { ZodFormattedError } from 'zod/v3'
+import type { User } from '@/modules/users/entities/User/User'
+
+const schema = z.object({
+  username: z.string(),
+  name: z.string().min(2, 'Nome é obrigatório'),
+  site: z.string().optional(),
+  bio: z.string().optional(),
+  phone: z.string().optional(),
+})
+
+interface UseUserUpdateOptions {
+  user: Ref<User | undefined>
+}
+
+export function useUserUpdate({ user: userRef }: UseUserUpdateOptions) {
+  const { logAndTrace } = useLogger()
+  const services = useServices()
+  const toast = useToast()
+  const loading = ref<boolean>()
+  const user = ref<User>()
+  const errors = ref<ZodFormattedError<User>>()
+
+  const safeParse = () => {
+    const result = schema.safeParse(user.value)
+    if (!result.success) {
+      errors.value = result.error.format() as ZodFormattedError<User>
+    }
+
+    return result
+  }
+
+  const update = async () => {
+    if (!user.value) return
+
+    loading.value = true
+
+    try {
+      await services.users.update(user.value.id, { ...user.value })
+
+      toast.add({
+        severity: 'info',
+        summary: 'Sucesso!',
+        detail: 'Dados atualizados',
+        life: 3000,
+      })
+    } catch (error) {
+      logAndTrace(error)
+    } finally {
+      loading.value = false
+    }
+  }
+
+  watchEffect(() => {
+    if (!userRef.value) {
+      return
+    }
+
+    user.value = userRef.value
+  })
+
+  return {
+    loading,
+    errors,
+    user,
+    safeParse,
+    update,
+  }
+}
