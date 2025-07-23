8e9567afbd928c9a477563bf41d26ae724a3c25d
pedrogutierresbr <pedro-gabriel1@hotmail.com>
2025-07-23 08:33:48 -0300

    Criação de rota para criar gist CreateNew dentro do módulo de gist

---
+++ modules/gists/screens/CreateNew/CreateNew.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { myselfKey } from '@/modules/users/composables/useMyself/useMyself'
+import type { MySelfContextProvider } from '@/modules/users/composables/useMyself/types'
+
+const { user, loading } = inject(myselfKey) as MySelfContextProvider
+</script>
+
+<template>
+  create new 
+  {{ user }}
+</template>
---
+++ pages/app/gist/create.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import CreateNew from '@/modules/gists/screens/CreateNew/CreateNew.vue'
+
+definePageMeta({
+  layout: 'admin',
+  middleware: ['auth']
+})
+</script>
+
+<template>
+  <CreateNew />
+</template>
